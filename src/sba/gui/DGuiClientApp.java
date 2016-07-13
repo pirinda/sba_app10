1ca42af335280c1fcb0dafff460b33890a95592e
Sergio Flores <sflores@swaplicado.com.mx>
2016-07-13 01:35:59 -0500

    Release 1.0 016.02: - Improvement of DPS view to sign, cancel and send documents properly.

--- src/sba/gui/DGuiClientApp.java
+++ src/sba/gui/DGuiClientApp.java
@@ -90,7 +90,7 @@ import sba.mod.trn.db.DDbDpsSeries;
 public class DGuiClientApp extends JFrame implements DGuiClient, ActionListener {
 
     public static final String APP_NAME = "SBA 1.0";
-    public static final String APP_RELEASE = "SBA 1.0 016.01";
+    public static final String APP_RELEASE = "SBA 1.0 016.02";
     public static final String APP_COPYRIGHT = "Copyright © 2011-2016 Sergio Abraham Flores Gutiérrez";
     public static final String APP_PROVIDER = "https://sites.google.com/site/iscsergioflores";
 
