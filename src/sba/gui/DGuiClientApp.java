eef969c7e142d629f0e4ef946b87f2e9466af22e
"Sergio Flores" <floresgtz@hotmail.com>
2024-11-08 18:09:20 -0600

    Release SBA 1.0 039.1

--- src/sba/gui/DGuiClientApp.java
+++ src/sba/gui/DGuiClientApp.java
@@ -96,7 +96,7 @@ import sba.mod.trn.db.DDbDpsSeries;
 public class DGuiClientApp extends JFrame implements DGuiClient, ActionListener {
 
     public static final String APP_NAME = "SBA 1.0";
-    public static final String APP_RELEASE = "SBA 1.0 039.0"; // release date: 2024-11-03
+    public static final String APP_RELEASE = "SBA 1.0 039.1"; // release date: 2024-11-05
     public static final String APP_COPYRIGHT = "Copyright © 2011-2024 Sergio Abraham Flores Gutiérrez";
     public static final String APP_PROVIDER = "https://sites.google.com/site/iscsergioflores";
 
